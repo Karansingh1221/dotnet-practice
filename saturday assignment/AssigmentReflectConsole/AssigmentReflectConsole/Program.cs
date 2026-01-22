03ae1dc8e021922cdee3398a125d16aa2075ed48
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-01-22 15:58:47 +0530

    Add files via upload

---
+++ saturday assignment/AssigmentReflectConsole/AssigmentReflectConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AssignmentDll;
+using System.Reflection;
+
+public class Program
+{
+    public static void Main()
+    {
+        string dllPath = @"C:\Users\Karan\source\repos\AssignmentDll\AssignmentDll\bin\Debug\net10.0\AssignmentDll.dll";
+
+        Assembly assembly = Assembly.LoadFrom(dllPath);
+
+        Console.WriteLine("Classes in DLL:\n");
+
+       
+        Console.WriteLine("Methods in dll");
+        foreach (Type t in assembly.GetTypes())
+        {
+            Console.WriteLine("Type: " + t.Name);
+
+            foreach (MethodInfo m in t.GetMethods())
+            {
+                Console.WriteLine("   Method: " + m.Name);
+            }
+
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ saturday assignment/AssignmentDll/AssignmentDll/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace AssignmentDll
+{
+    public interface I1
+    {
+        void M1();
+        void M2();
+    }
+    public interface I2
+    {
+        void M3();
+    }
+    public interface I3
+    {
+        void M4();
+    }
+    public class ClassA:I1
+    {
+        public void M1()
+        {
+            Console.WriteLine("M1 method called");
+        }
+        public void M2()
+        {
+            Console.WriteLine("M2 method called");
+        }
+        public void M3()
+        {
+            Console.WriteLine("M3 method called");
+        }
+        public void M4()
+        {
+            Console.WriteLine("M4 method called");
+        }
+    }
+    public class ClassB : ClassA
+    {
+        public void M5()
+        {
+            Console.WriteLine("M5 method called");
+        }
+    }
+}
