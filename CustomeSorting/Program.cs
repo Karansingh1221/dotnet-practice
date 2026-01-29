438ff8cb96e5492311d3000501b2954d73fe9be3
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-01-29 14:59:08 +0530

    Update Program.cs

--- CustomeSorting/Program.cs
+++ CustomeSorting/Program.cs
@@ -14,17 +14,25 @@ class Student{
         return $"name: {Name},marks: {Marks},age: {Age}";
     }
 }
+class StudentComparer:IComparer<Student>{
+    public int Compare(Student x,Student y){
+        int mcomp=y.Marks.CompareTo(x.Marks);
+        if(mcomp!=0) return mcomp;
+        return x.Age.CompareTo(y.Age);
+    }
+}
 class Program{
     public static void Main(string[] args){
         List<Student>  repo=new List<Student>{new Student("Sparsh",84,20),new Student("Karan",95,21)
         ,new Student("Jagriti",94,20),new Student("Anushka",94,22)};
-        repo=repo.OrderByDescending(std=>std.Age).ToList();
-        repo=repo.OrderBy(std=>std.Marks).ToList();
-        repo.Reverse();
+        // repo = repo.OrderByDescending(std => std.Marks).ThenBy(std => std.Age).ToList();
+        // foreach(var i in repo){
+        //     Console.WriteLine(i);
+        // }
+
+        repo.Sort(new StudentComparer());
         foreach(var i in repo){
             Console.WriteLine(i);
         }
-
-
     }
 }
