b9c97a2eed4cab0ded7f673d3e0a5d277e8ce01e
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-02-04 15:47:28 +0530

    Add files via upload

---
+++ TopBrainsQuestions/C#Question10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Program{
+    public static void Main(string[] args){
+        Console.WriteLine("Enter the height in (cm):");
+        int height=int.Parse(Console.ReadLine());
+        string category="";
+        if(height<150){
+            category="short";
+        }else if(height>=150 && height<180){
+            category="average";
+        }else if(height>=180){
+            category="tall";
+        }
+        Console.WriteLine("Height category="+category);
+    }
+}
---
+++ TopBrainsQuestions/C#Question19/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+class Employee{
+    public virtual double Payroll(){
+        return 0;
+    }
+}
+class HourlyEmployee:Employee{
+    public int Hours{get;set;}
+    public int Rate{get;set;}
+    public HourlyEmployee(int h,int r){
+        Hours=h;
+        Rate=r;
+    }
+    public override double Payroll(){
+        return (double)Hours*Rate;
+    }
+}
+class SalariedEmployee:Employee{
+    public double Salary{get;set;}
+    public SalariedEmployee(double s){
+        Salary=s;
+    }
+    public override double Payroll(){
+        return Salary;
+    }
+}
+class ComissionEmployee:Employee{
+    public double BaseSalary{get;set;}
+    public double Comission{get;set;}
+    public ComissionEmployee(double b,double c){
+        BaseSalary=b;
+        Comission=c;
+    }
+    public override double Payroll(){
+        return BaseSalary+Comission;
+    }
+}
+public class Program{
+    public static void Main(string[] args){
+        string[] arr={"he","he","se","ce","se","ce"};
+        double total_payroll=0;
+        foreach(string s in arr){
+            Console.WriteLine("Enter the information of the employee");
+            objectCreate(s,ref total_payroll);
+        }
+        Console.WriteLine($"Total Payroll of all the employees is {total_payroll:F1}");
+    }
+    public static void objectCreate(string s,ref double total_payroll){
+        if(s=="he"){
+            Console.WriteLine("Enter Hours Worked:");
+            int h=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Rate: ");
+            int r=int.Parse(Console.ReadLine());
+            HourlyEmployee he=new HourlyEmployee(h,r);
+            total_payroll+=he.Payroll();
+        }else if(s=="ce"){
+            Console.WriteLine("Enter Base Salary:");
+            double sa=double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Comission: ");
+            double c=double.Parse(Console.ReadLine());
+            ComissionEmployee e=new ComissionEmployee(sa,c);
+            total_payroll+=e.Payroll();
+        }else if(s=="se"){
+            Console.WriteLine("Enter Monthly Salary:");
+            double ms=double.Parse(Console.ReadLine());
+            SalariedEmployee e=new SalariedEmployee(ms);
+            total_payroll+=e.Payroll();
+        }
+    }
+}
