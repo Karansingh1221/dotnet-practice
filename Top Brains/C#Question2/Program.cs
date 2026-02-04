823a6d72c4fe910431e55352e7755d6ed93e8577
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-02-04 15:33:42 +0530

    Add files via upload

---
+++ Top Brains/C#Question2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+public class Program{
+    public static void Main(string[] args){
+        string fast="Karan";
+        string last="singh";
+        
+    }
+}
