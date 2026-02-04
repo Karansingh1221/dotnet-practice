c05252177b3ee707be87449d8515d671bcaf1011
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-02-04 15:44:26 +0530

    Add files via upload

---
+++ TopBrainsQuestions/C#Question3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+public class MainClass{
+    public static void Main(string[] args){
+        string str=Console.ReadLine();
+        Dictionary<char,int> dict=new Dictionary<char,int>();
+        str=str.Trim();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.Length;i++){
+            sb.Append(str[i]);
+            while(i<str.Length-1 && str[i]==str[i+1]){
+                i++;
+            }
+        }
+        if(sb.Length==0){
+            Console.WriteLine("Empty String ");
+            return;
+        }
+        sb[0]=char.ToUpper(sb[0]);
+        for(int i=1;i<sb.Length;i++){
+            if(sb[i-1]==' '){
+
+                sb[i]=char.ToUpper(sb[i]);
+            }
+        }
+        str=sb.ToString();
+        Console.WriteLine(str);
+    }
+}
