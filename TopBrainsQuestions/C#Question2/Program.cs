579e2b299f9aba514782fb0488001dcd16d2aedc
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-02-05 15:55:16 +0530

    Update Program.cs
    Added functionality to read two strings and process them by removing certain characters and filtering duplicates.

--- TopBrainsQuestions/C#Question2/Program.cs
+++ TopBrainsQuestions/C#Question2/Program.cs
@@ -1,10 +1,33 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 public class Program{
     public static void Main(string[] args){
-        string fast="Karan";
-        string last="singh";
-        
+        string first=Console.ReadLine();
+        string last=Console.ReadLine();
+        if(first.Length>50 || last.Length>50){
+            Console.WriteLine("Input string can not be greater than 50 characters");
+            return;
+        }
+        string vow="aeiou";
+        for(int i=0;i<first.Length;i++){
+            char ch=first[i];
+            if(!vow.Contains(char.ToLower(ch)) && last.Contains(ch.ToString(),StringComparison.OrdinalIgnoreCase)){
+                first=first.Remove(i,1);
+                i--;
+            }
+        }
+        string res="";
+        if(first.Length==0){
+            Console.WriteLine("");
+            return;
+        }
+        res+=first[0];
+        for(int i=1;i<first.Length;i++){
+            if(first[i]!=first[i-1]){
+                res+=first[i];
+            }
+        }
+        Console.WriteLine(res);
     }
 }
