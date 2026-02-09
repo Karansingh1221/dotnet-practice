fd34888987f685df6cee57bc58805e4c0602e61c
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-02-09 20:38:19 +0530

    Add files via upload

---
+++ TopBrainsQuestions/C#Question8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program{
+    public static double CalculateArea(double rad){
+        double area=Math.PI*rad*rad;
+        return Math.Round(area,2,MidpointRounding.AwayFromZero);
+    }
+    public static void Main(string[] args){
+        Console.WriteLine("Enter Radius:");
+        double radius=double.Parse(Console.ReadLine());
+        double Area=CalculateArea(radius);
+        Console.WriteLine($"Area: {Area}");
+    }
+}
