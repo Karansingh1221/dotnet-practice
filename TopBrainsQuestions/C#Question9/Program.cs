73f8b4193bf9342f122b63350403e9119a82a994
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2026-02-09 20:20:32 +0530

    Add files via upload

---
+++ TopBrainsQuestions/C#Question9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program{
+    public static double convertToCm(int f){
+        double cm=(double) f*30.48;
+        return Math.Round(cm,2,MidpointRounding.AwayFromZero);
+    }
+    public static void Main(string[] args){
+        Console.WriteLine("Enter foot");
+        int foot=int.Parse(Console.ReadLine());
+        double cm=convertToCm(foot);
+        Console.WriteLine($"{cm}");
+    }
+}
